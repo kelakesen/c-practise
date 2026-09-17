18f09ddea3c6e0ea91fcf80d235ae6cead6f4289
gaojinglong <2112621496@qq.com>
2026-09-17 10:08:30 +0800

    ex29

---
+++ ex_practise/ex29/src/libex29.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "dbg.h"
+
+
+int print_a_message(const char *msg)
+{
+    printf("A STRING: %s\n", msg);
+
+    return 0;
+}
+
+
+int uppercase(char *msg)
+{
+    int i = 0;
+
+    int len = strlen(msg);
+    msg[len] = '\0';
+    // BUG: \0 termination problems
+    for(i = 0; msg[i] != '\0'; i++) {
+        printf("%c", toupper(msg[i]));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+int lowercase(char *msg)
+{
+    int i = 0;
+
+    int len = strlen(msg);
+    msg[len] = '\0';
+    // BUG: \0 termination problems
+    for(i = 0; msg[i] != '\0'; i++) {
+        printf("%c", tolower(msg[i]));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+int fail_on_purpose(const char *msg)
+{
+    return 1;
+}
